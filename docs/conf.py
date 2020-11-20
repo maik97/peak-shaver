f30cfff8102679e3faff8652cdf78bbefc1324c3
maik97 <35041716+maik97@users.noreply.github.com>
2020-11-20 16:07:34 +0100

    added title

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'peak-shaver'
-copyright = '2020, Maik SChürmann'
+copyright = '2020, Maik Schürmann'
 author = 'Maik SChürmann'
 
 # The full version, including alpha/beta/rc tags
