fad87887a782e1aa310f51225f534df02f99b879
maik97 <35041716+maik97@users.noreply.github.com>
2020-11-21 13:58:00 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, Maik Schürmann'
 author = 'Maik SChürmann'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -44,7 +44,7 @@ language = 'python'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-autodoc_mock_imports = ['tensorflow', 'stable_baselines']
+autodoc_mock_imports = ['tensorflow', 'stable_baselines', 'seaborn', 'keras']
 
 # -- Options for HTML output -------------------------------------------------
 
