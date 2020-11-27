0b8d694b23bd5273088102168f985869120159a9
maik97 <35041716+maik97@users.noreply.github.com>
2020-11-27 17:43:31 +0100

    Update example_data_preprocessing.py

--- peak-shaver/example_data_preprocessing.py
+++ peak-shaver/example_data_preprocessing.py
@@ -1,7 +1,7 @@
 '''
 Example of preprocessing the HIPE-Dataset.
 '''
-from main.schaffer import mainDataset, lstmInputDataset
+from main.schaffer import mainDataset, lstmInputDataset, DatasetStatistics
 from main.common_func import wait_to_continue
 
 main_dataset_creator = mainDataset(D_PATH='_small_d/', period_string_min='15min', full_dataset=True)
@@ -19,7 +19,7 @@ wait_to_continue()
 
 main_dataset_creator.norm_activation_time_df()
 wait_to_continue()
-'''
+
 
 lstm_dataset_creator = lstmInputDataset(D_PATH='_small_d/', period_string_min='15min',full_dataset=False,
                                         num_past_periods=12, drop_main_terminal=False, use_time_diff=True,
@@ -37,3 +37,10 @@ wait_to_continue()
 
 lstm_dataset_creator.sequence_training_data(num_seq_periods=12)
 wait_to_continue()
+'''
+
+plotter = DatasetStatistics(D_PATH='_small_d/', period_string_min='15min', full_dataset=True)
+
+#plotter.plot_dist_power('test1')
+#plotter.plot_dist_power_change('test2')
+plotter.plot_compare_peak_lenghts('test3', show_plot=False)
