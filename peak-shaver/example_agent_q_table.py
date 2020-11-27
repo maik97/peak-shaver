5999bec43dbf94c2d712a99dc5b49da03807bd44
maik97 <35041716+maik97@users.noreply.github.com>
2020-11-27 23:33:47 +0100

    Update example_agent_q_table.py

--- peak-shaver/example_agent_q_table.py
+++ peak-shaver/example_agent_q_table.py
@@ -1,157 +1,143 @@
 '''
 Example of an RL-Agent that uses the basic Q-Table.
 '''
-import sys
-import numpy as np
-import pandas as pd
-import random
-import h5py
-
-import gym
-from gym import spaces
-
 from datetime import datetime
 from collections import deque
-from tqdm import tqdm
 
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.optimizers import Adam
-
-import main.schaffer as schaffer
-from main.wahrsager import wahrsager, max_seq, mean_seq
+from main.schaffer import mainDataset, lstmInputDataset
+from main.wahrsager import wahrsager
+from main.common_func import max_seq, mean_seq
 from main.common_env import common_env
 from main.reward_maker import reward_maker
+
+# Import the Q-Table agent: 
 from main.agent_q_table import Q_Learner
 
-# Logging-Namen:
-now            = datetime.now()
-NAME           = 'Q_Table'+now.strftime("_%d-%m-%Y_%H:%M:%S")
-DATENSATZ_PATH = '_BIG_D/'
+# Naming the agent and setting up the directory path:
+now    = datetime.now()
+NAME   = 'Q_Table'+now.strftime("_%d-%m-%Y_%H:%M:%S")
+D_PATH = '_BIG_D/'
 
-# Lade Dataframe:
-df = schaffer.alle_inputs_neu()[24:-12]
+# Load the dataset:
+main_dataset = mainDataset(
+    D_PATH=D_PATH,
+    period_string_min='5min',
+    full_dataset=True)
 
-#df['pred_mean']       = wahrsager(TYPE='MEAN').pred()[:-12]
-#df['pred_max']        = wahrsager(TYPE='MAX').pred()[:-12]
-#df['pred_normal']     = wahrsager(TYPE='NORMAL').pred()[:-12]
-#df['pred_max_labe']   = wahrsager(TYPE='MAX_LABEL_SEQ').pred()
-#df['pred_mean_label'] = wahrsager(TYPE='MEAN_LABEL_SEQ').pred()
+# Normalized dataframe:
+df = main_dataset.make_input_df(
+    drop_main_terminal=False,
+    use_time_diff=True,
+    day_diff='holiday-weekend')
 
-prediction_seq        = wahrsager(TYPE='SEQ', num_outputs=12).pred()
-df['max_pred_seq']    = max_seq(prediction_seq)
-#df['mean_pred_seq']   = mean_seq(prediction_seq)
+# Sum of the power demand dataframe (nor normalized):
+power_dem_arr = main_dataset.load_total_power()
 
-power_dem_arr  = schaffer.load_total_power()[24:-12]
+# Load the LSTM input dataset:
+lstm_dataset = lstmInputDataset(main_dataset,df)
 
-# Lade Reward-Maker:
-R_HORIZON = 0
-r_maker        = reward_maker(
-                    COST_TYPE               = 'exact_costs',     # 'yearly_costs', 'max_peak_focus'
-                    R_TYPE                  = 'savings_focus',   #'costs_focus', 'savings_focus'
-                    M_STRATEGY              = 'single_step',              # None, 'sum_to_terminal', 'average_to_neighbour', 'recurrent_to_Terminal'
-                    R_HORIZON               = None,         # 'episode', 'single_step', integer for multi-step
-                    cost_per_kwh            = 0.2255,  # in €
-                    LION_Anschaffungs_Preis = 34100,   # in €
-                    LION_max_Ladezyklen     = 1000,
-                    SMS_Anschaffungs_Preis  = 115000/3,# in €
-                    SMS_max_Nutzungsjahre   = 20,      # in Jahre
-                    Leistungspreis          = 102,     # in €
-                    focus_peak_multiplier   = 4        # multiplier for max_peak costs
-                    )
+# Making predictions:
+normal_predictions = wahrsager(lstm_dataset, TYPE='NORMAL').pred()[:-12]
+seq_predictions    = wahrsager(lstm_dataset, TYPE='SEQ', num_outputs=12).pred()
 
-# Lade Environment:
-env            = common_env(
-                    df                   = df,
-                    power_dem_arr        = power_dem_arr,
-                    input_list           = ['norm_total_power','max_pred_seq'],
-                    DATENSATZ_PATH       = DATENSATZ_PATH,
-                    NAME                 = NAME,
-                    max_SMS_SoC          = 12,
-                    max_LION_SoC         = 54,
-                    PERIODEN_DAUER       = 5,
-                    ACTION_TYPE          = 'discrete',
-                    num_discrete_obs     = 21,
-                    num_discrete_actions = 22,
-                    #action_space         = spaces.Discrete(22), # A ∈ [0,1]
-                    #observation_space    = spaces.Box(low=0, high=21, shape=(4,1), dtype=np.float16),
-                    reward_maker         = r_maker
-                    )
+# Adding the predictions to the dataset:
+df            = df[24:-12]
+df['normal']  = normal_predictions
+df['seq-max'] = max_seq(seq_predictions)
 
-# Initilisiere Parameter für Target-Network
-update_num       = 1
-update_counter   = 0
 
-# Inititialsiere Epoch-Parameter:
+# Number of warm-up steps:
+num_warmup_steps = 100
+# Train every x number of steps:
+update_num       = 500
+# Number of epochs and steps:
 epochs           = 1000
 epochs_len       = len(df)
+# Horizon for Multi-Step-Rewards and/or LSTM-Implementation:
+horizon = 0
 
-num_warmup_steps = 100
-warmup_counter   = 0
+# Setup reward_maker
+r_maker = reward_maker(
+    COST_TYPE               = 'exact_costs',
+    R_TYPE                  = 'savings_focus',
+    M_STRATEGY              = 'single_step',
+    cost_per_kwh            = 0.2255,
+    LION_Anschaffungs_Preis = 34100,
+    LION_max_Ladezyklen     = 1000,
+    SMS_Anschaffungs_Preis  = 115000/3,
+    SMS_max_Nutzungsjahre   = 20,
+    Leistungspreis          = 102)
 
-# Init Agent Parameter
+# Setup common_env
+env = common_env(
+    df                   = df,
+    power_dem_arr        = power_dem_arr,
+    input_list           = ['norm_total_power','max_pred_seq'],
+    DATENSATZ_PATH       = DATENSATZ_PATH,
+    NAME                 = NAME,
+    max_SMS_SoC          = 12,
+    max_LION_SoC         = 54,
+    PERIODEN_DAUER       = 5,
+    ACTION_TYPE          = 'discrete',
+    OBS_TYPE             = 'discrete',
+    num_discrete_obs     = 21,
+    num_discrete_actions = 22,
+    reward_maker         = r_maker)
 
+# Setup agent:
+Agent = Q_Learner(
+    env            = env,
+    memory         = deque(maxlen=(update_num+horizon)),
+    # Training-Parameter:
+    gamma          = 0.85,
+    epsilon        = 0.8,
+    epsilon_min    = 0.1,
+    epsilon_decay  = 0.999996,
+    lr             = 0.5,
+    tau            = 0.125,
+    # jede Dimension jeweils ∈ [0,0.05,...,1]
+    Q_table        = np.zeros((22,22,22,22,22)))
 
-# Init Agent:
-Agent          = Q_Learner(
-                    env            = env,
-                    #memory         = deque(maxlen=(update_num)),
-                    memory         = deque(maxlen=(R_HORIZON+update_num)),
-
-                    gamma          = 0.85,
-                    epsilon        = 0.8,
-                    epsilon_min    = 0.1,
-                    epsilon_decay  = 0.999996,
-                    lr             = 0.5,
-                    tau            = 0.125,
-
-                    Q_table        = np.zeros((22,22,22,22,22)), # jede Dimension jeweils ∈ [0,0.05,...,1]
-                    )
 
 print('Warmup-Steps per Episode:', num_warmup_steps)
 print('Training for',epochs,'Epochs')
 
 for e in range(epochs):
-    #print('Epoch:', e)
-    #tqdm.write('Starting Epoch: {}'.format(e))
+
     cur_state = env.reset()
-    #cur_state = cur_state.reshape(1,len(cur_state))[0]
-    cur_state = cur_state.reshape(len(cur_state),1).tolist()
+    update_counter   = 0
+    warmup_counter   = 0
 
-    #tqdm.write('Warm-Up for {} Steps...'.format(num_warmup_steps))
     while warmup_counter < num_warmup_steps:
-        action, epsilon            = Agent.act(cur_state)
-        new_state, reward, done, step_counter_episode, _ = env.step(action, epsilon)
-        new_state                  = new_state.reshape(len(cur_state),1).tolist()
-        Agent.remember(cur_state, action, reward, new_state, done, step_counter_episode)
+        action, epsilon                 = Agent.act(cur_state)
+        new_state, reward, done, sce, _ = env.step(action, epsilon)
+        new_state                       = new_state
+        Agent.remember(cur_state, action, reward, new_state, done, sce)
 
-        cur_state                  = new_state
-        warmup_counter            += 1
+        cur_state = new_state
+        warmup_counter += 1
 
-    bar = tqdm(range(epochs_len))#, leave=True, file=sys.stdout)
-    bar.set_description("Training - Epoch {}".format(e))
-    for step in bar:
+    for step in range(epochs_len):
 
-        action, epsilon            = Agent.act(cur_state)
-        new_state, reward, done, step_counter_episode, _ = env.step(action, epsilon)
-        new_state                  = new_state.reshape(len(cur_state),1).tolist()            
+        action, epsilon                 = Agent.act(cur_state)
+        new_state, reward, done, sce, _ = env.step(action, epsilon)
+        new_state                       = new_state
         Agent.remember(cur_state, action, reward, new_state, done, step_counter_episode)
         
-        cur_state                  = new_state
+        cur_state = new_state
         
         if done == False:
-            index_len = update_num
+            training_num = update_num
         else:
-            index_len = update_num + R_HORIZON
+            training_num = update_num + R_HORIZON
 
         update_counter += 1
         if update_counter == update_num or done == True:
-            Agent.replay(index_len)
+            Agent.replay(training_num)
             update_counter = 0
 
         if done:
             break
-    #bar.clear()
 
     if e % 10 == 0:
         Agent.save_agent(NAME, DATENSATZ_PATH, e)
