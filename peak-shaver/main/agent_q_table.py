3542e31fdf943a4e4fc168151cbd272f0eb0536c
maik97 <35041716+maik97@users.noreply.github.com>
2020-11-29 02:22:06 +0100

    Update agent_q_table.py

--- peak-shaver/main/agent_q_table.py
+++ peak-shaver/main/agent_q_table.py
@@ -10,13 +10,12 @@ from gym import spaces
 
 from datetime import datetime
 from collections import deque
-from tqdm import tqdm
 
-import schaffer
-from wahrsager import wahrsager
-from common_env import common_env
-from reward_maker import reward_maker
-from common_func import try_training_on_gpu, max_seq, mean_seq
+try:
+    from main.common_func import try_training_on_gpu, AgentStatus
+except:
+    from common_func import try_training_on_gpu, AgentStatus
+
 
 class Q_Learner:
     """
@@ -33,24 +32,46 @@ class Q_Learner:
         tau (float): Factor for copying weights from model network to target network
         Q_table (array): Initial Q-Table, all values should be set to zero
     """
-    def __init__(self, env, memory, gamma=0.85, epsilon=0.8, epsilon_min=0.1, epsilon_decay=0.999996, lr=0.5, tau=0.125, Q_table=np.zeros((22,22,22,22,22))):
+    def __init__(self, env, memory_len, gamma=0.85, epsilon=0.8, epsilon_min=0.1, epsilon_decay=0.999996, lr=0.5, tau=0.125, Q_table=np.zeros((22,22,22,22,22)), load_table=None):
 
         self.env            = env
-        self.memory         = memory
-        
+
+        self.D_PATH         = self.env.__dict__['D_PATH']
+        self.NAME           = self.env.__dict__['NAME']
+
         self.gamma          = gamma
         self.epsilon        = epsilon
         self.epsilon_min    = epsilon_min
-        self.epsilon_decay  = epsilon_decay
+        self.epsilon_decay  = epsilon_decay # string:'linear' oder float
+        self.epsilon_og     = epsilon
         self.lr             = lr
         self.tau            = tau
 
         self.Q_table        = Q_table # jede Dimension jeweils ∈ [0,0.05,...,1]
+        self.LOGGER         = self.env.__dict__['LOGGER']
+        
+        self.horizon = self.env.__dict__['reward_maker'].__dict__['R_HORIZON']
+        if isinstance(self.horizon, int) == True:
+            memory_len += self.horizon
+        else:
+            self.horizon = 0
+        self.memory = deque(maxlen=memory_len)
 
-        # Init Logging
-        #self.LOGGER            = logger.Logger(DATENSATZ_PATH+'LOGS/agent_logging/'+NAME)
+        if self.env.__dict__['ACTION_TYPE'] != 'discrete':
+            raise Exception("Q-Table-Agent can not use continious values for the actions. Change 'ACTION_TYPE' to 'discrete'!")
 
-    def act(self, state):
+        if self.env.__dict__['OBS_TYPE'] != 'discrete':
+            raise Exception("Q-Table-Agent can not use continious values for the observations. Change 'OBS_TYPE' to 'discrete'!")
+
+        if load_table != None:
+            with h5py.File(D_PATH+'agent-models/'+load_table, 'r') as hf:
+                self.Q_table = hf[:][:]
+
+    def init_agent_status(self, epochs, epoch_len):
+        self.agent_status = AgentStatus(epochs*epoch_len)
+
+
+    def act(self, state, random_mode=False):
         '''
         Function, in which the agent decides an action, either from greedy-policy or from prediction. Use this function when iterating through each step.
         
@@ -64,16 +85,20 @@ class Q_Learner:
             
             epsilon (float): Current (decayed) epsilon
         '''
-        self.epsilon *= self.epsilon_decay
+        if self.epsilon_decay == 'linear':
+            self.epsilon = self.epsilon_og*(self.env.__dict__['sum_steps']/self.agent_status.__dict__['max_steps'])
+        else:
+            self.epsilon *= self.epsilon_decay
         self.epsilon = max(self.epsilon_min, self.epsilon)
-        if np.random.random() < self.epsilon:
-            return self.env.action_space.sample(), self.epsilon  # = random action, zufallsparameter
+        
+        if np.random.random() < self.epsilon or random_mode==True:
+            return self.env.action_space.sample()  # = random action, zufallsparameter
         
         # state[0] : Power_demand
         # state[1] : SoC_SMS
         # state[2] : SoC_LiON
-        state = state.reshape(len(cur_state),1).tolist()
-        return np.argmax(self.Q_table[state][0]), self.epsilon # = action, zufallsparameter
+        state = state.reshape(len(state),1).tolist()
+        return np.argmax(self.Q_table[state][0]) # = action, zufallsparameter
 
     def remember(self, state, action, reward, new_state, done, step_counter_episode):
         '''
@@ -87,18 +112,19 @@ class Q_Learner:
             done (bool): If True the episode ends
             step_counter_episode (integer): Episode step at which the action was performed
         '''
-        state = state.reshape(len(cur_state),1).tolist()
-        new_state = new_state.reshape(len(cur_state),1).tolist()
+        state = state.reshape(len(state),1).tolist()
+        new_state = new_state.reshape(len(new_state),1).tolist()
         self.memory.append([state, action, reward, new_state, done, step_counter_episode])
 
-    def replay(self, index_len):
+    def replay(self, batch_size):
         '''
         Training-Process for the DQN from past steps. Use this function after a few iteration-steps (best use is the number of index_len). Alternatively use this function at each step.
 
         Args:
             index_len (integer): Number of past states to learn from
         '''
-        for i in range(index_len):
+        loss_list = []
+        for i in range(batch_size):
             # Lade Step von Simulation
             state, action, reward, new_state, done, step_counter_episode = self.memory[i] 
             
@@ -116,19 +142,27 @@ class Q_Learner:
             state_and_action = state_and_action.reshape(len(state_and_action),1).tolist()
             
             Q_target = self.Q_table [state_and_action]
-            self.Q_table[state_and_action] += (self.lr * (reward + (Q_future * self.gamma) - Q_target))
-                    
+            loss = (self.lr * (reward + (Q_future * self.gamma) - Q_target))
+            self.Q_table[state_and_action] += loss
+            loss_list.append(loss) 
 
-    def save_agent(self, NAME, DATENSATZ_PATH, e):
+        name        = self.env.__dict__['NAME']
+        epoch       = self.env.__dict__['episode_counter']
+        total_steps = self.env.__dict__['sum_steps']
+        self.agent_status.print_agent_status(name, epoch, total_steps, batch_size, self.epsilon, np.mean(loss_list))         
+        self.LOGGER.log_scalar('Loss:', loss, total_steps, done)
+        self.LOGGER.log_scalar('Epsilon:', self.epsilon, total_steps, done)
+
+
+    def save_agent(self, e):
         '''For saving the agents model at specific epoch. Make sure to not use this function at each epoch, since this will take up your memory space.
 
         Args:
             NAME (string): Name of the model
-            DATENSATZ_PATH (string): Path to save the model
             e (integer): Takes the epoch-number in which the model is saved
         '''
-        with h5py.File(DATENSATZ_PATH+'tables/'+NAME+'_{}.h5'.format(e), 'w') as hf:
-            hf.create_dataset(NAME,  data=self.Q_table)
+        with h5py.File(self.D_PATH+'agent-models/'+self.NAME+'_{}.h5'.format(e), 'w') as hf:
+            hf.create_dataset(self.NAME, data=self.Q_table)
 
 
 
@@ -136,10 +170,16 @@ def main():
     '''
     Example of an RL-Agent that uses the basic Q-Table.
     '''
+    import main.schaffer
+    from main.wahrsager import wahrsager
+    from main.common_env import common_env
+    from main.reward_maker import reward_maker
+    from main.common_func import try_training_on_gpu, max_seq, mean_seq
+
     # Logging-Namen:
     now            = datetime.now()
     NAME           = 'Q_Table'+now.strftime("_%d-%m-%Y_%H:%M:%S")
-    DATENSATZ_PATH = '_BIG_D/'
+    D_PATH = '_BIG_D/'
 
     # Lade Dataframe:
     df = schaffer.alle_inputs_neu()[24:-12]
@@ -177,7 +217,7 @@ def main():
                         df                   = df,
                         power_dem_arr        = power_dem_arr,
                         input_list           = ['norm_total_power','max_pred_seq'],
-                        DATENSATZ_PATH       = DATENSATZ_PATH,
+                        D_PATH       = D_PATH,
                         NAME                 = NAME,
                         max_SMS_SoC          = 12,
                         max_LION_SoC         = 54,
@@ -266,7 +306,7 @@ def main():
         #bar.clear()
 
         if e % 10 == 0:
-            Agent.save_agent(NAME, DATENSATZ_PATH, e)
+            Agent.save_agent(NAME, D_PATH, e)
 
 
 if __name__ == "__main__":
