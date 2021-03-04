31e682c98a98b93574d09e23c21f95856c154050
maik97 <35041716+maik97@users.noreply.github.com>
2021-03-04 11:16:17 +0100

    Update tuning_wahrsager.py

--- peak-shaver/tuning_wahrsager.py
+++ peak-shaver/tuning_wahrsager.py
@@ -6,221 +6,88 @@ from main.schaffer import lstmInputDataset
 # Load the dataset:
 df, power_dem_df, main_dataset = basic_dataset()
 
-def standart_settings():
-    '''Creates standart results to compare the tests'''
 
-    print('Testing standart_settings()')
-    lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=12)
+def run_wahrsager(NAME,TYPE='NORMAL',num_outputs=1, dropout=0.1, num_hidden=3,lstm_size=256, num_past_periods=24,
+                  activation_hidden='relu',activation_end='relu',lr=0.001,num_epochs=1000):
+
+    #try:
+    lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=num_past_periods)
     normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-        TYPE = 'NORMAL', 
-        NAME ='_test_standart',
-        #PLOTTING = True,
+        TYPE = TYPE, 
+        NAME = NAME,
+        PLOTTING = True,
         # Model-Parameter:
-        num_outputs       = 1,
-        dropout           = 0.1,
-        recurrent_dropout = 0.1,
-        num_hidden        = 3,
-        lstm_size         = 128,
-        first_hidden_size = 128,
+        num_outputs       = num_outputs,
+        dropout           = dropout,
+        recurrent_dropout = dropout,
+        num_hidden        = num_hidden,
+        lstm_size         = lstm_size,
+        first_hidden_size = lstm_size,
         neuron_num_change = 0.5,
-        activation_hidden = 'relu',
-        activation_end    = 'relu',
-        lr                = 0.001,
+        activation_hidden = activation_hidden,
+        activation_end    = activation_end,
+        lr                = lr,
         # Trainings-Parameter
         val_data_size     = 2000,
-        num_epochs        = 200,
+        num_epochs        = num_epochs,
         ).train()
+    #except Exception as e:
+    #   print(e)
 
+def parameter_tuning(num_runs=3):
 
-def test_learning_rate():
-    '''Tests different learning rates'''
-
-    print('Testing test_learning_rate()')
-    learning_rate_list = [0.00001,0.0001,0.001,0.01,0.1,0.5]
-
-    lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=12)
-    
-    for learning_rate in learning_rate_list:
-        normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-            TYPE       = 'NORMAL', 
-            NAME       = '_test_learning_rate_{}'.format(learning_rate),
-            lr         = learning_rate,
-            num_epochs = 1000,
-            PLOTTING   = True,
-           
-        ).train()
-
-
-def test_dropout():
-    '''Tests overfitting by comparing training loss to validation loss'''
-
-    print('Testing test_dropout()')
-    dropout_list = [0.1,0.2,0.4,0.6]
-
-    lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=12)
-    
-    for dropout in dropout_list:
+    for i in range(num_runs):
         
-        normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-            TYPE              = 'NORMAL', 
-            NAME              ='_test_dropout_{}'.format(dropout),
-            dropout           = dropout,
-            recurrent_dropout = dropout,
-            num_epochs        = 1000,
-            PLOTTING          = True,
-            ).train()
+        run_wahrsager('_test_standart')
 
+        learning_rate_list = [0.0001,0.001,0.01] 
+        for learning_rate in learning_rate_list:
+            run_wahrsager('_test_learning_rate_{}'.format(learning_rate), lr=learning_rate)
 
+        dropout_list = [0,0.2,0.3]
+        for dropout in dropout_list:
+            run_wahrsager('_test_dropout_{}'.format(dropout), dropout=dropout)
 
-def test_sigmoid():
-    ''' Tests difference between relu and sigmoid activation function'''
-    
-    print('Testing test_sigmoid()')
-    lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=12)
-    normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-        TYPE = 'NORMAL', 
-        NAME ='_test_sigmoid',
-        activation_hidden = 'sigmoid',
-        activation_end    = 'sigmoid',
-        PLOTTING = True,
-        ).train()
-
-
-def test_lstm_layers():
-    '''Tests difference between different numbers of lstm layers'''
-
-    print('Testing test_lstm_layers()')
-    lstm_layers_list = [16,32,64,256,512,1028]
-
-    lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=12)
-    
-    for lstm_layers in lstm_layers_list:
+        activation_list = [['relu','sigmoid'],['sigmoid','sigmoid'],['sigmoid','relu']] 
+        for activation in activation_list:
+            run_wahrsager('_test_activation_{}-{}'.format(activation[0],activation[1]), activation_hidden=activation[0], activation_end=activation[1])
         
-        normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-            TYPE = 'NORMAL', 
-            NAME ='_test_lstm_layers_{}'.format(lstm_layers),
-            # Model-Parameter:
-            lstm_size         = lstm_layers,
-            first_hidden_size = lstm_layers,
-            PLOTTING = True,
-            ).train()
+        lstm_layers_list = [64,128,512]
+        for lstm_layers in lstm_layers_list:
+            run_wahrsager('_test_lstm_layers_{}'.format(lstm_layers), lstm_size=lstm_layers)
 
-
-def test_hidden_layers():
-    '''Tests difference between different numbers of hidden layers'''
-
-    print('Testing test_hidden_layers()')
-    hidden_layers_list = [1,2]
-
-    lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=12)
-    
-    for hidden_layers in hidden_layers_list:
+        hidden_layers_list = [1,2]
+        for hidden_layers in hidden_layers_list:
+            run_wahrsager('_test_hidden_layers_{}'.format(hidden_layers), num_hidden=hidden_layers)
         
-        normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-            TYPE = 'NORMAL', 
-            NAME ='_test_hidden_layers_{}'.format(hidden_layers),
-            num_hidden = hidden_layers,
-            PLOTTING = True,
-            ).train()
+        past_periods_list = [6,12,30] 
+        for past_periods in past_periods_list:
+            run_wahrsager('_test_past_periods_{}'.format(past_periods), num_past_periods=past_periods)
 
+        mean_list = [6,12,24] 
+        for mean_ in mean_list:
+            run_wahrsager('_test_mean_{}'.format(mean_), num_past_periods=mean_)
 
-def test_past_periods():
-    ''' Tests difference between different numbers of past periods'''
-    
-    print('Testing test_past_periods()')
-    past_periods_list = [6,24]
-
-    for past_periods in past_periods_list:
+        max_list = [6,12,24] 
+        for max_ in max_list:
+            run_wahrsager('_test_max_{}'.format(max_), num_past_periods=max_)
         
-        lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=past_periods)
-        normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-            TYPE = 'NORMAL', 
-            NAME ='_test_past_periods_{}'.format(past_periods),
-            PLOTTING = True,
-            ).train()
-
-
-def test_mean():
-    ''' Tests difference between different numbers of past periods'''
-
-    print('Testing test_mean()')
-    past_periods_list = [6,12,24]
-
-    for past_periods in past_periods_list:
+        max_label_seq_list = [6,12,24]
+        for max_label_seq in max_label_seq_list:
+            run_wahrsager('_test_max_label_seq_{}'.format(max_label_seq), TYPE='MAX_LABEL_SEQ', num_outputs=max_label_seq)
         
-        lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=past_periods)
-        normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-            TYPE = 'MEAN', 
-            NAME ='_test_mean_{}'.format(past_periods),
-            #PLOTTING = True,
-            ).train()
-
-
-def test_max():
-    ''' Tests difference between different numbers of past periods'''
-
-    print('Testing test_max()')
-    past_periods_list = [6,12,24]
-
-    for past_periods in past_periods_list:
+        mean_label_seq_list = [6,12,24]
+        for mean_label_seq in mean_label_seq_list:
+            run_wahrsager('_test_mean_label_seq_{}'.format(mean_label_seq), TYPE='MEAN_LABEL_SEQ', num_outputs=mean_label_seq)
         
-        lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=past_periods)
-        normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-            TYPE = 'MAX', 
-            NAME ='_test_max_{}'.format(past_periods),
-            #PLOTTING = True,
-            ).train()
+        seq_list = [6,12,24]
+        for seq in seq_list:
+            run_wahrsager('_test_seq_{}'.format(seq), TYPE='SEQ', num_outputs=seq)
 
 
-def test_max_label_seq():
-    ''' Tests difference between different numbers of past periods'''
 
-    print('Testing test_max_label_seq()')
-    output_periods_list = [6,12,24]
+parameter_tuning()
 
-    for output_periods in output_periods_list:
-        
-        lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=12)
-        normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-            TYPE = 'MAX_LABEL_SEQ', 
-            NAME ='_test_max_label_seq_{}'.format(output_periods),
-            num_outputs = output_periods,
-            #PLOTTING = True,
-            ).train()
-
-
-def test_mean_label_seq():
-    ''' Tests difference between different numbers of past periods'''
-
-    print('Testing test_mean_label_seq()')
-    output_periods_list = [6,12,24]
-
-    for output_periods in output_periods_list:
-        
-        lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=12)
-        normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-            TYPE = 'MEAN_LABEL_SEQ', 
-            NAME ='_test_mean_label_seq_{}'.format(output_periods),
-            num_outputs = output_periods,
-            #PLOTTING = True,
-            ).train()
-
-
-def test_seq():
-    ''' Tests difference between different numbers of past periods'''
-
-    print('Testing test_seq()')
-    output_periods_list = [6,12,24]
-
-    for output_periods in output_periods_list:
-        
-        lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=12)
-        normal_predictions = wahrsager(lstm_dataset, power_dem_df,
-            TYPE = 'SEQ', 
-            NAME ='_test_seq_{}'.format(output_periods),
-            num_outputs = output_periods,
-            #PLOTTING = True,
-            ).train()
 
 '''
 standart_settings()
@@ -257,8 +124,8 @@ except Exception as e:
     print(e)
 '''
 
+'''
 def final_normal():
-    '''Creates standart results to compare the tests'''
 
     print('Testing final_normal()')
     lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=24)
@@ -283,7 +150,6 @@ def final_normal():
         ).train()
 
 def final_seq():
-    '''Creates standart results to compare the tests'''
 
     print('Testing final_seq()')
     lstm_dataset = lstmInputDataset(main_dataset, df, num_past_periods=24)
@@ -312,3 +178,4 @@ def final_seq():
 for i in range(3):
 	#final_normal()
 	final_seq()
+'''
